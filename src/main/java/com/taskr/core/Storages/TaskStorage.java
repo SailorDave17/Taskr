54baf40bff1ae39b5c4fdc16469a27c9c714afc8
Jane Wetherholt <janewetherholt@gmail.com>
2020-11-30 10:27:02 -0500

    Started on task storage

--- src/main/java/com/taskr/core/Storages/TaskStorage.java
+++ src/main/java/com/taskr/core/Storages/TaskStorage.java
@@ -1,7 +1,30 @@
 package com.taskr.core.Storages;
 
+import com.taskr.core.Resources.Task;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskStorage {
+
+//    private TaskRepository taskRepo;
+//
+//    public TaskStorage(TaskRepository taskRepo) {
+//        this.taskRepo = taskRepo;
+//    }
+//
+//    public void save(Task task) {
+//        taskRepo.save(task);
+//    }
+//
+//    public void deleteById(Long id) {
+//        taskRepo.deleteById(id);
+//    }
+//
+//    public Iterable<Task> findAll() {
+//        return taskRepo.findAll();
+//    }
+//
+//    public Task findById(Long id) {
+//        if(taskRepo.findById(id))
+//    }
 }

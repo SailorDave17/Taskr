c9ad45804c25152adf662a6dfeb7aa11ec6a30cf
Millicent Duke <millicent.j.duke@gmail.com>
2020-12-01 15:17:37 -0500

    Created task assignment endpoint for testing

--- back-end/src/main/java/com/taskr/core/Controllers/UserController.java
+++ back-end/src/main/java/com/taskr/core/Controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.taskr.core.Controllers;
 
+import com.taskr.core.Resources.Task;
+import com.taskr.core.Resources.TaskTemplate;
 import com.taskr.core.Resources.User;
+import com.taskr.core.Storages.TaskStorage;
+import com.taskr.core.Storages.TaskTemplateStorage;
 import com.taskr.core.Storages.UserStorage;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,14 +12,29 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private UserStorage userStorage;
+    private TaskTemplateStorage taskTemplateStorage;
+    private TaskStorage taskStorage;
 
-    public UserController(UserStorage userStorage) {
+    public UserController(UserStorage userStorage, TaskTemplateStorage taskTemplateStorage, TaskStorage taskStorage) {
         this.userStorage = userStorage;
+        this.taskTemplateStorage = taskTemplateStorage;
+        this.taskStorage = taskStorage;
     }
 
     @GetMapping("/api/user/{id}")
     public User retrieveUserById(@PathVariable Long id) {
         return userStorage.findById(id);
+    }
+
+    //Not necessary in the final project
+    //TODO remove this once auto-assign algorithm exists.
+    @PatchMapping("/api/user/{userId}/assign-task")
+    public User assignTaskToUser(@PathVariable Long userId, @RequestBody Long taskTemplateId){
+        User user = userStorage.findById(userId);
+        TaskTemplate taskTemplate = taskTemplateStorage.findById(taskTemplateId);
+        Task newTask = new Task(user, taskTemplate);
+        taskStorage.save(newTask);
+        return user;
     }
 
     @PostMapping("/api/user/new")

d145bc9db9b9c75ac189f2ddd7b3521ec759a325
Lauren Monowar-Jones <lvjones214@aol.com>
2020-12-03 11:06:33 -0500

    finished single user view, but NOT tested

--- front-end/Js/singleuserview.js
+++ front-end/Js/singleuserview.js
@@ -1,9 +1,9 @@
-const displaySingleUserView = function (user) {
+const displaySingleUserView = function(user) {
     const userPageHeader = document.querySelector(".user-page-header")
     clearChildren(userPageHeader);
     const userNamePageElement = document.createElement("h1");
     userNamePageElement.classList.add("username");
-    userNamePageElement.innerText = ${ user.name } + `'s Task List - ` + ${ day } //whatever day is being accessed by the user. default will be Sunday
+    userNamePageElement.innerText = $ { user.name } + `'s Task List - ` + $ { day } //whatever day is being accessed by the user. default will be Sunday
     const userIcon = document.createElement("img");
     userIcon.classList.add("user-page-icon");
     userPageHeader.appendChild(userNamePageElement);
@@ -12,13 +12,13 @@ const displaySingleUserView = function (user) {
 
     const listOfTasks = document.createElement("div");
     listOfTasks.classList.add("user-task-list");
-    userPageHeader.appendChild(listOfTasks);
+    userPageHeader.append(listOfTasks);
     user.tasks.day.forEach(task => {
         const taskStickyNote = document.createElement("div")
         taskStickyNote.classList.add("chores-list");
         const checkBox = document.createElement("input");
         checkBox.classList.add("chore-done");
-        if(task.done== true){
+        if (task.done == true) {
             checkBox.innerHTML = `<input type="checkbox" checked class="chore-done" id="check-chore">`
         }
         const choreName = document.createElement("label");
@@ -41,5 +41,31 @@ const displaySingleUserView = function (user) {
         listOfTasks.appendChild(taskDuration); //not sure if appending them all to the same thing is the right choice//
 
     });
-}
 
+    const displayProgressBar = document.createElement("progress");
+    displayProgressBar.classList.add("user-progress-bar");
+    displayProgressBar.setAttribute("value", $ { user.percentDone });
+    listOfTasks.append(displayProgressBar);
+
+    const displayWeeklyCalendar = document.createElement("div");
+    displayWeeklyCalendar.classList.add("calendar");
+    displayProgressBar.append(displayWeeklyCalendar);
+
+    user.task.day.forEach(day => {
+        const dayOfTheWeek = document.createElement("div");
+        dayOfTheWeek.classList.add("day Sunday");
+        dayOfTheWeek.setAttribute("id", "Sunday");
+        dayOfTheWeek.innerText = "Sunday";
+        const howManyTasks = document.createElement("p");
+        howManyTasks.classList.add("how-many-tasks-on-day");
+        howManyTasks.innerText = "You have " + user.tasks.day.count + " on " + day;
+        const clickHereMessage = document.createElement("p");
+        clickHereMessage.classList.add("click-here-message");
+        clickHereMessage.innerText = "Click here to see this day's tasks.";
+        displayWeeklyCalendar.appendChild(dayOfTheWeek);
+        displayWeeklyCalendar.appendChild(howManyTasks);
+        displayWeeklyCalendar.appendChild(clickHereMessage);
+    })
+
+
+}

aa310c12223a3192a1edeb3f4aa75a61d7f3fced
Josephgathuru <Gathuru.joseph@gmail.com>
2020-12-03 15:21:28 -0500

    started app.js

--- front-end/app.js
+++ front-end/app.js
@@ -1,3 +1,26 @@
 import{
-    singleuserview.js
-}
+    createHeader
+} from "./front-end/Js/header.js"
+
+import{
+    displaySingleUserView 
+} from "./front-end/Js/singleuserview.js"
+
+import{
+    createFooter
+} from "./front-end/Js/footer.js"
+
+const container = document.queryselector ('container');
+
+container.prepend(createHeader());
+const userNamePageElement = document.createElement("h1");
+userNamePageElement.classList.add("username");
+container.appendChild(userNamePageElement);
+
+
+fetch("http://localhost:8080/api/users")
+.then(response => response.json())
+.then(users => displaySingleUserView(users))
+.catch(error => console.log (error));
+
+container.appendChild(createFooter())

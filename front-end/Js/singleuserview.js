d0a55a62e9ec60dd03dafce536520ea6eb37ce22
Ryan Archer <ryanarcheryeah@gmail.com>
2020-12-06 11:35:51 -0500

    Fixed stickyNote layout and style and created dummy JSON that will be deleted

--- front-end/Js/singleuserview.js
+++ front-end/Js/singleuserview.js
@@ -1,52 +1,58 @@
+import{
+    allUsers
+} from "./sampleAllUserJson.js"
 
-
-const displaySingleUserView = function(user) {
+const displaySingleUserView = function(users) {
     const mainElement = document.createElement("main");
     mainElement.classList.add("main-content");
     const userPageHeader = document.createElement("div");
     userPageHeader.classList.add("user-page-header");
-    userPageHeader.innerText = "Electric Boogaloo";
-    // userPageHeader.innerText = `${user.name}'s Task List -${day}` //whatever day is being accessed by the user. default will be Sunday
     // clearChildren(userPageHeader);
     const userNamePageElement = document.createElement("h1");
     userNamePageElement.classList.add("username");
-    userNamePageElement.innerText = user.name;
-    // const userIcon = document.createElement("img");
-    // userIcon.classList.add("user-page-icon");
+    userNamePageElement.innerText = "Mom's Task List-Sunday";
+    // userNamePageElement.innerText = `${user.name}'s Task List -${day}` //whatever day is being accessed by the user. default will be Sunday
+    const userIcon = document.createElement("img");
+    userIcon.classList.add("user-page-icon");
+    userIcon.setAttribute("src","/front-end/images/woman_1f469.png")
     mainElement.appendChild(userPageHeader);
     userPageHeader.appendChild(userNamePageElement);
-    // userPageHeader.appendChild(userIcon);
+    userPageHeader.appendChild(userIcon);
 
 
-    // const listOfTasks = document.createElement("div");
-    // listOfTasks.classList.add("user-task-list");
-    // userPageHeader.append(listOfTasks);
-    // user.tasks.day.forEach(task => {
-    //     const taskStickyNote = document.createElement("div")
-    //     taskStickyNote.classList.add("chores-list");
-    //     const checkBox = document.createElement("input");
-    //     checkBox.classList.add("chore-done");
-    //     if (task.done == true) {
-    //         checkBox.innerHTML = `<input type="checkbox" checked class="chore-done" id="check-chore">`
-    //     }
-    //     const choreName = document.createElement("label");
-    //     choreName.classList.add("chore-name")
-    //     choreName.innerText = task.title;
-    //     checkBox.setAttribute("id", "check-chore");
-    //     choreName.setAttribute("for", "check-chore");
-    //     const taskInfoList = document.createElement("ul")
-    //     const taskDueDate = document.createElement("li");
-    //     taskDueDate.classList.add("task-due-date");
-    //     taskDueDate.innerText = task.dueBy;
-    //     const taskDuration = document.createElement("li");
-    //     taskDuration.classList.add("task-duration");
-    //     taskDuration.innerText = task.minutesExpectedToComplete;
-    //     listOfTasks.appendChild(taskStickyNote);
-    //     listOfTasks.appendChild(checkBox);
-    //     listOfTasks.appendChild(choreName);
-    //     listOfTasks.appendChild(taskInfoList);
-    //     listOfTasks.appendChild(taskDueDate);
-    //     listOfTasks.appendChild(taskDuration); //not sure if appending them all to the same thing is the right choice//
+    const listOfTasks = document.createElement("div");
+    listOfTasks.classList.add("user-task-list");
+    userPageHeader.append(listOfTasks);
+    // user.taskList.forEach(task => {
+        const taskStickyNote = document.createElement("div")
+        taskStickyNote.classList.add("chores-list");
+
+        const checkBox = document.createElement("input");
+        checkBox.classList.add("chore-done");
+        // if (task.done === true) {
+        //     checkBox.innerHTML = `<input type="checkbox" checked class="chore-done" id="check-chore">`
+        // }
+        const choreName = document.createElement("label");
+        choreName.classList.add("chore-name")
+        // choreName.innerText = task.title;
+        choreName.innerText = "Vacuum Family Room";
+        checkBox.setAttribute("id", "check-chore");
+        choreName.setAttribute("for", "check-chore");
+        const taskInfoList = document.createElement("ul")
+        const taskDueDate = document.createElement("li");
+        taskDueDate.classList.add("task-due-date");
+        // taskDueDate.innerText = taskList.dueBy;
+        taskDueDate.innerText = "Sunday";
+        const taskDuration = document.createElement("li");
+        taskDuration.classList.add("task-duration");
+        // taskDuration.innerText = taskList.minutesExpectedToComplete;
+        taskDuration.innerText = "30 minutes";
+        listOfTasks.appendChild(taskStickyNote);
+        taskStickyNote.appendChild(checkBox);
+        taskStickyNote.appendChild(choreName);
+        taskStickyNote.appendChild(taskInfoList);
+        taskStickyNote.appendChild(taskDueDate);
+        taskStickyNote.appendChild(taskDuration); //not sure if appending them all to the same thing is the right choice//
 
     // });
 

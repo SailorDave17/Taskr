9d9370d1b1cd42b79fc9f311d6088b6951bc32c3
Dave <HSC.Coach@gmail.com>
2020-12-08 10:06:51 -0500

    fixed package name

--- back-end/src/main/java/com/taskr/core/WebConfig.java
+++ back-end/src/main/java/com/taskr/core/WebConfig.java
@@ -1,4 +1,4 @@
-package org.wcci.campuslibraries;
+package com.taskr.core;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;

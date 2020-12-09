cd51663d9eb72f74ff9f2d2ab9610db75b9e9b50
Lauren Monowar-Jones <lvjones214@aol.com>
2020-12-09 13:13:57 -0500

    save work

--- front-end/Js/customizetasksview.js
+++ front-end/Js/customizetasksview.js
@@ -20,8 +20,27 @@ const displayCustomizeTasksView = function(tasks) {
     multipleUserFrequency.innerText = "Multiple User Option";
     const dropButtonUser1 = document.createElement("button");
     dropButtonUser1.classList.add("dropbtn-user");
-    dropButtonUser1.setAttribute("id", user.name);
-
-
+    dropButtonUser1.setAttribute("id", "mom");
+    dropButtonUser1.innerText = "Mom"
+        //dropButtonUser1.setAttribute("id", user.name);
+        //dropButtonUser1.innerText = user.name;
+    const dropButtonUser2 = document.createElement("button");
+    dropButtonUser2.classList.add("dropbtn-user");
+    dropButtonUser2.setAttribute("id", "dad");
+    dropButtonUser2.innerText = "Dad"
+        //dropButtonUser2.setAttribute("id", user.name);
+        //dropButtonUser2.innerText = user.name;
+    const dropButtonUser3 = document.createElement("button");
+    dropButtonUser3.classList.add("dropbtn-user");
+    dropButtonUser3.setAttribute("id", "bro");
+    dropButtonUser3.innerText = "Bro"
+        //dropButtonUser1.setAttribute("id", user.name);
+        //dropButtonUser1.innerText = user.name;
+    const dropButtonUser4 = document.createElement("button");
+    dropButtonUser4.classList.add("dropbtn-user");
+    dropButtonUser4.setAttribute("id", "sis");
+    dropButtonUser4.innerText = "Sis"
+        //dropButtonUser2.setAttribute("id", user.name);
+        //dropButtonUser2.innerText = user.name;
 
 }

ad67ebea2cc58691aae9d647f36f433818cf6383
Ryan Archer <ryanarcheryeah@gmail.com>
2020-12-10 10:14:43 -0500

    fixed a dueby api error

--- front-end/app.js
+++ front-end/app.js
@@ -16,12 +16,12 @@ container.prepend(displayHeader());
 const mainElement = document.createElement("main");
 mainElement.classList.add("main-content");
 container.appendChild(mainElement);
-// const userNamePageElement = document.createElement("h1");
+// const userNamePageElement = document.createElement("h1"); 
 // userNamePageElement.classList.add("username");
 // container.appendChild(userNamePageElement);
 
 
-fetch("http://localhost:8080/api/user/3")
+fetch("http://localhost:8080/api/user/2")
     .then(response => response.json())
     //.then(json => console.log(json))
     .then(user => displaySingleUserView(user))

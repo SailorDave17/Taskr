2affa0128ede1dd293eea751d4d2f0561b124b8f
Millicent Duke <millicent.j.duke@gmail.com>
2020-12-10 15:20:08 -0500

    Revert app.js to Joseph's last push

--- front-end/app.js
+++ front-end/app.js
@@ -17,35 +17,21 @@ import {
 const container = document.querySelector('.container');
 
 container.prepend(displayHeader());
-// This is the path to the single user view.
-const mainElement = document.createElement("main");
-mainElement.classList.add("main-content");
-container.appendChild(mainElement);
 
 //This is the path to the all user view.
-// const allUsersMainElement = document.createElement("main");
-// allUsersMainElement.classList.add("all-users-main");
-// container.appendChild(allUsersMainElement);
+const allUsersMainElement = document.createElement("main");
+allUsersMainElement.classList.add("all-users-main");
+container.appendChild(allUsersMainElement);
 
-const userNamePageElement = document.createElement("h1");
-userNamePageElement.classList.add("username");
-container.appendChild(userNamePageElement);
 
 
 fetch("http://localhost:8080/api/users")
     .then(response => response.json())
-    //.then(json => console.log(json))
-    .then(user => displaySingleUserView(user))
-    .then(singleUserElement => mainElement.appendChild(singleUserElement))
+    // .then(json => console.log(json))
+    .then(users => displayAllUsersView(users))
+    .then(allUsersView => allUsersMainElement.appendChild(allUsersView))
     .catch(error => console.log(error));
 
 
-// fetch("http://localhost:8080/api/users")
-//     .then(response => response.json())
-//     //.then(json => console.log(json))
-//     .then(users => displayAllUsersView(users))
-//     .then(allUsersView => allUsersMainElement.appendChild(allUsersView))
-//     .catch(error => console.log(error));
 
-
-container.appendChild(createFooter())
+container.appendChild(createFooter());

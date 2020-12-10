087c6cdfc5d399eac89a85bc45f84fa33bb610c8
Munir Sefer <munirsefer@gmail.com>
2020-12-10 14:10:37 -0500

    progress bar now works

--- front-end/Js/allusersview.js
+++ front-end/Js/allusersview.js
@@ -11,38 +11,39 @@ const displayAllUsersView = function(users) {
     allUsersMainElement.classList.add("all-users-main");
     const userTiles = document.createElement("section");
     userTiles.classList.add("user-tiles");
-    // userTiles.innerText = "Electric Boogaloo 2: Rendering the View";
-    // clearChildren(userTiles);
     users.forEach(user => {
     const singleUserTile = document.createElement("div");
     singleUserTile.classList.add("single-user-tile");
     singleUserTile.setAttribute("id" , user.userColor);
+    singleUserTile.addEventListener("click", ()=>{
+        clearChildren(allUsersMainElement);
+        fetch("http://localhost:8080/api/user/" + user.id)
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .then(user => displaySingleUserView(user))
+    .then(singleUserElement => mainElement.appendChild(singleUserElement))
+    .catch(error => console.log(error));
+    })
     const singleUserName = document.createElement("h1");
     singleUserName.classList.add("single-user-name");
     singleUserName.innerText = user.name;
     const singleUserIcon = document.createElement("img");
     singleUserIcon.classList.add("user-icon");
-    // const userIcon = "/front-end/images/mom.png";
-    // if(user.userIcon === "sis.ico") {
-    //     userIcon = "front-end/images/sis.png";
-    // }
-    // if(user.userIcon === "dad.ico") {
-    //     userIcon = "front-end\images\Dad.png";
-    // }
     singleUserIcon.setAttribute("src" , user.userIcon);
-    // const tasksAssigned = document.createElement("p");
-    // tasksAssigned.classList.add("tasks-assigned");
-    // tasksAssigned.innerText = "number of tasks";
-    // const progressBarElement = document.createElement("progress");
-    // progressBarElement.classList.add("progress-bar");
-    // const numberOfTaskDone = 2;
-    // const userPercentTaskDone = numberOfTaskDone/tasksAssigned;
-    // progressBarElement.setAttribute("value", userPercentTaskDone);
-    // progressBarElement.setAttribute("max", "100");
+    const tasksAssigned = document.createElement("p");
+    tasksAssigned.classList.add("tasks-assigned");
+    tasksAssigned.innerText = "Number of Tasks: " + user.numberTasksAssigned;
+    const progressBarElement = document.createElement("progress");
+    progressBarElement.classList.add("progress-bar");
+    const userPercentTaskDone = user.numberTasksCompleted*100/user.numberTasksAssigned;
+    progressBarElement.setAttribute("value", userPercentTaskDone);
+    progressBarElement.setAttribute("max", "100");
     allUsersMainElement.appendChild(userTiles);
     userTiles.appendChild(singleUserTile);
     singleUserTile.appendChild(singleUserName);
     singleUserTile.appendChild(singleUserIcon);
+    singleUserTile.appendChild(tasksAssigned);
+    singleUserTile.appendChild(progressBarElement);
 
         
     }); 
@@ -52,6 +53,11 @@ const displayAllUsersView = function(users) {
 
     return allUsersMainElement;
 
+}
+const clearChildren = function (element) {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
 }
 
 
